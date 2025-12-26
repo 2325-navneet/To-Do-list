4cb02318220d1f289aafe6422a337288088171bc
Navneet Kumar <navneetrajput1414@gmail.com>
2025-12-26 12:32:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+//IEFE
+(() => {
+    //state variable
+    let toDoListArry = []
+    //ui variable 
+    const form = document.querySelector(".form");
+    const input = form.querySelector(".form__input");
+    const ul = document.querySelector(".toDoList");
+
+    //event listener 
+    form.addEventListener('submit', e => {
+        // prevent default behaviour - Page reload
+        e.preventDefault();
+        //give item a unique ID
+        let itemId = String(Date.now());
+        //get/assign input value
+        let toDoItem = input.value;
+        //pass ID and item into functions
+        addItemToDOM(itemId,toDoItem)
+        addItemToArray(itemId,toDoItem)
+        // clear the input box. (this is default behaviour but we got rid of that)
+        input.value = '';
+    });
+
+    ul.addEventListener('click', e => {
+    let id = e.target.getAttribute('data-id')
+    if (!id) return // user clicked in something else
+    //pass id through to functions
+    removeItemFromDOM(id);
+    removeItemFromArray(id);
+  });
+
+    //Functions
+    function addItemToDOM(itemId , toDoItem){
+        //create a li
+        const li = document.createElement('li')
+        li.setAttribute("data-id",itemId);
+        //add toDo item text to li 
+        li.innerText = toDoItem
+        //add li to a DOM
+        ul.appendChild(li);
+    }
+
+    function addItemToArray(itemId ,toDoItem ){
+        // add item to array as an object with an ID so we can find and delete it later
+        toDoListArry.push({itemId,toDoItem})
+        console.log(toDoListArry)
+    }
+
+    function removeItemFromDOM(id) {
+    // get the list item by data ID
+    var li = document.querySelector('[data-id="' + id + '"]');
+    // remove list item
+    ul.removeChild(li);
+  }
+
+  function removeItemFromArray(id) {
+    // create a new toDoListArray with all li's that don't match the ID
+    toDoListArray = toDoListArray.filter(item => item.itemId !== id);
+    console.log(toDoListArray);
+  }
+
+})();
